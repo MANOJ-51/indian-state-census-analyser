b84279ecb686f85d43200ca330fbb61510985a71
MANOJ-51 <m4anoj51@gmail.com>
2022-07-15 00:45:12 +0530

    displaying welcome message

---
+++ src/main/java/com/bridgelabz/StateCensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class StateCensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("Welcome to state census analyser");
+    }
+}
